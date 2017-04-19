47ca7d82ae10f66e35a8b5aa3628c85637cffc46
Eddy Drew <eddy@xcap.ca>
2017-04-19 22:04:19 +0000

    some work

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,48 @@
 var express = require('express');
 
+
 module.exports = function(myReddit) {
     var authController = express.Router();
     
     authController.get('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        response.render("login-form");
     });
     
-    authController.post('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
-    });
     
+ authController.post('/login', function(request, response) {
+        //console.log(request.body);
+
+        myReddit.checkUserLogin(request.body.username, request.body.password)   //boots up our server and calls checkUserLogin function
+        .then(result => { 
+            return myReddit.createUserSession(result.id)    //return the user's session id promise
+        })
+        .then(token => {                            
+        //console.log(token)
+            response.cookie('SESSION', token);      //express syntax for storing cookie from the response?
+        })
+        .then(useless => {                      //bring user back to the home page)
+            response.redirect('/')
+            
+        })            
+        .catch( error => { //response.render('error', {error: error})
+            response.status(401).send('401 Unauthorized.')
+        })
+    });
+ 
+   
     authController.get('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        response.render('signup-form');
     });
+  
     
     authController.post('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        myReddit.createUser({
+            username: request.body.username,
+            password: request.body.password
+            }).then(result => {
+                response.redirect("/auth/login");
+            }); 
+        //  response.send();
     });
     
     return authController;
