ab00297edd33c1a20bf31763c32d0638a7734859
Halotitee <moreseed01@gmail.com>
2017-04-20 00:11:59 +0000

    finished Signup and Login part

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,20 +4,40 @@ module.exports = function(myReddit) {
     var authController = express.Router();
     
     authController.get('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        response.render('login-form.pug');
     });
     
     authController.post('/login', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        //console.log(request.body);
+        myReddit.checkUserLogin(request.body.username, request.body.password)
+        .then(result => {                                   //login success
+            return myReddit.createUserSession(result.id);   
+        })
+        .then(sessionId => {                            //set a cookie for user login
+            response.cookie('SESSION', sessionId);          
+        })
+        .then(reDirect => {
+            response.redirect('/');                     //back to homepage
+        })
+        .catch(error => {                               //login unsuccessful
+            response.status(401).send('401 Unauthorized');
+        });
+        
     });
     
     authController.get('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        response.render('signup-form');                             
     });
     
     authController.post('/signup', function(request, response) {
-        response.send("TO BE IMPLEMENTED");
+        myReddit.createUser(request.body)
+        .then(response.redirect('/auth/login')
+        );
     });
     
     return authController;
 }
+
+
+//response.render   -- will call the Pug template engine
+//response.redirect -- prevents some duplicate form submission
